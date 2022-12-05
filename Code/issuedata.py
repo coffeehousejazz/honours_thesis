ad48bc51626e67bc9ad0674fe42811fd103d5029
Jasmine Mishra <72532236+coffeehousejazz@users.noreply.github.com>
2022-12-05 11:45:12 -0800

    ive done it

--- Code/issuedata.py
+++ Code/issuedata.py
@@ -20,9 +20,9 @@ repo_name = "Metalhead.jl"
 mentee_url = f"https://api.github.com/users/{mentee_username}"
 # Writing mentee data to .json
 #send get request
-response = requests.get(mentee_url, auth=(username,token))
+response = requests.get(mentee_url, auth=(username,token)).text
 #save to object
-data = response.json()
+data = json.loads(response)
 json_serial = json.dumps(data, indent=4)
 json_writer("mentee", json_serial)
     
@@ -39,8 +39,8 @@ mentions_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/issues?me
 response2 = requests.get(mentions_url, auth=(username,token)).text
 #save to object
 data2 = json.loads(response2)
-json_serial = json.dumps(data2, indent=4)
-json_writer("mentions", json_serial)
+json_serial2 = json.dumps(data2, indent=4)
+json_writer("mentions", json_serial2)
     
 ### CREATOR ###
 #api url to grab created
@@ -50,29 +50,30 @@ creator_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/issues?cre
 response3 = requests.get(creator_url, auth=(username,token)).text
 #save to object
 data3 = json.loads(response3)
-json_serial = json.dumps(data3, indent=4)
-json_writer("creator", json_serial)
+json_serial3 = json.dumps(data3, indent=4)
+json_writer("creator", json_serial3)
 
 # read all the issue numbers into a list
 issue_nums = []
-for v, k in data2.items():
-    print(v)
-    if v == "number":
-       issue_nums.append(k)
+for dic in data2:
+    for key in dic:
+        if key == "number":
+            issue_nums.append(dic[key])
+for dic in data3:
+    for key in dic:
+        if key == "number":
+            issue_nums.append(dic[key])
 
-for v, k in data3.items():
-    print(v)
-    if v == "number":
-       issue_nums.append(k)
-       
+# remove duplicates
 issue_nums = list(dict.fromkeys(issue_nums))
 
 # get all the comments data into json files
 for num in issue_nums:
+    # issue number into api url
    comments_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/issues/{num}/comments"
    #send get request
    response4 = requests.get(creator_url, auth=(username,token)).text
    #save to object
    data4 = json.loads(response4)
    json_serial = json.dumps(data4, indent=4)
-   json_writer("creator", json_serial)
+   json_writer(f"{num}comments", json_serial)
