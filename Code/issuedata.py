39f7f2fe648214397228de0d1235f265d01e17f2
Jasmine Mishra <72532236+coffeehousejazz@users.noreply.github.com>
2022-12-04 21:57:12 -0800

    more stuff

--- Code/issuedata.py
+++ Code/issuedata.py
@@ -1,25 +1,46 @@
 import requests, json
-from prettytable import PrettyTable
 
-table = PrettyTable()
-table.field_names = ["key", "value"]
+def json_responses(url):
+    #send get request
+    response = requests.get(url)
+    #save to object
+    data = response.json()
+    # Serializing json
+    return json.dumps(data, indent=4)
 
-github_username  = "khatrivinay1"
+## input info about project
+mentee_username  = "theabhirath"
+#mentor_usernames = ["", ""]
+owner_name = "FluxML"
+repo_name = "Metalhead.jl"
 
-#api url to grab mentions
-api_url = f"https://api.github.com/repos/FluxML/Metalhead.jl/issues?mentions={github_username}"
-
-#send get request
-response = requests.get(api_url)
-
-# save the json to mongoDB
-
-#look at the data in terminal
-data =  response.json()
-
-# Serializing json
-json_object = json.dumps(data, indent=4)
- 
-# Writing to .json
-with open("/Users/jasminemishra/Desktop/mentor.json", "w") as outfile:
+### USER DATA ###
+#api url to grab mentee user data
+mentee_url = f"https://api.github.com/users/{mentee_username}"
+# Writing mentee data to .json
+json_object = json_responses(mentee_url)
+with open("/Users/jasminemishra/Desktop/mentee.json", "w") as outfile:
     outfile.write(json_object)
+    
+#api url to grab mentor user data
+#for i in mentor_usernames:
+#    mentor_url = f"https://api.github.com/users/{i}"
+
+### . ###
+
+### MENTIONS ###
+#api url to grab mentions
+issue_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/issues?mentions={mentee_username}"
+# Writing mentions to .json
+json_object = json_responses(issue_url)
+with open("/Users/jasminemishra/Desktop/mentions.json", "w") as outfile:
+    outfile.write(json_object)
+    
+# read all the issues into a list
+issue_nums = []
+
+issue_nums.append()
+
+# get all the comments data into json files
+for num in issue_nums:
+    
