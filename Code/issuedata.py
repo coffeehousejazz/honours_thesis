2742d958eba69304e6966e6910fd4b9e9f5e9eae
Jasmine Mishra <72532236+coffeehousejazz@users.noreply.github.com>
2022-12-05 00:13:19 -0800

    http auth and save properly

--- Code/issuedata.py
+++ Code/issuedata.py
@@ -1,16 +1,14 @@
 import requests, json
-
-def json_responses(url):
-    #send get request
-    response = requests.get(url)
-    #save to object
-    data = response.json()
-    # Serializing json
-    return json.dumps(data, indent=4)
+from requests.auth import HTTPBasicAuth
 
 def json_writer(name, json_object):
-    with open(f"/Users/jasminemishra/Desktop/{name}.json", "w") as outfile:
+    with open(f"/Users/jasminemishra/Desktop/data/responses/{name}.json", "w") as outfile:
         outfile.write(json_object)
+
+# AUTHENTICATION
+username = 'coffeehousejazz'
+token = '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'
+requests.get('https://api.github.com/search/repositories?q=github+api', auth=HTTPBasicAuth(username,token))
 
 ## input info about project and run !!
 mentee_username  = "theabhirath"
@@ -22,34 +20,51 @@ repo_name = "Metalhead.jl"
 #api url to grab mentee user data
 mentee_url = f"https://api.github.com/users/{mentee_username}"
 # Writing mentee data to .json
-json_object = json_responses(mentee_url)
-json_writer("mentee", json_object)
+#send get request
+response = requests.get(mentee_url)
+#save to object
+data = response.json()
+json_serial = json.dumps(data, indent=4)
+json_writer("mentee", json_serial)
+print(type(data))
     
 #api url to grab mentor user data
 #for i in mentor_usernames:
 #    mentor_url = f"https://api.github.com/users/{i}"
-
 ### . ###
 
 ### MENTIONS ###
 #api url to grab mentions
-issue_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/issues?mentions={mentee_username}"
-# Writing mentions to .json
-json_object = json_responses(issue_url)
-json_writer("mentions", json_object)
+mentions_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/issues?mentions={mentee_username}"
+#writing mentions to .json
+#send get request
+response2 = requests.get(mentions_url).text
+#save to object
+data2 = json.loads(response2)
+json_serial = json.dumps(data2, indent=4)
+json_writer("mentions", json_serial)
+print(type(data2))
     
 ### CREATOR ###
 #api url to grab created
 creator_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/issues?creator={mentee_username}"
 # Writing created to .json
-json_object2 = json_responses(creator_url)
-json_writer("creator", json_object2)
-    
+#send get request
+response3 = requests.get(creator_url).text
+#save to object
+data3 = json.loads(response3)
+json_serial = json.dumps(data3, indent=4)
+json_writer("creator", json_serial)
+print(type(data3))
+
 # read all the issue numbers into a list
 issue_nums = []
+for v, k in data.items():
+    if v == "number":
+       issue_nums.append(k)
 
 # get all the comments data into json files
-for num in issue_nums:
-    comments_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/issues/{num}/comments"
-    json_object = json_responses(comments_url)
-    json_writer(f"{num}comments", json_object)
+# for num in issue_nums:
+#    comments_url = f"https://api.github.com/repos/{owner_name}/{repo_name}/issues/{num}/comments"
+#    json_object = json_responses(comments_url)
+#    json_writer(f"{num}comments", json_object)
