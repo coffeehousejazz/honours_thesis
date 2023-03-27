9ae013179fcf1fdcb6c54403833d613dbb20cda0
Jasmine Mishra <72532236+coffeehousejazz@users.noreply.github.com>
2023-03-26 21:13:35 -0700

    fixes ! changes! amazing!

--- Code/annotate/add_field.py
+++ Code/annotate/add_field.py
@@ -4,8 +4,8 @@ from pymongo import MongoClient
 client = MongoClient("mongodb+srv://HonourThesis:XZJXwB8NNdHIoxGw@cluster0.no1barz.mongodb.net/test")
 
 # select database and collection
-db = client["GSoC21"]
-collection = db["CommentsREAL"]
+db = client["GSoC"]
+collection = db["Users"]
 
 # add a new field to all documents in the collection
-collection.update_many({}, {"$set": {"GSoC": "2021"}})
+collection.update_many({}, {"$set": {"GSoC": "2022"}})
