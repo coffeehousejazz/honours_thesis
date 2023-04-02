91efb2557b34b58e053ce752dd63107462e314e4
Jasmine Mishra <72532236+coffeehousejazz@users.noreply.github.com>
2023-04-01 22:18:18 -0700

    change

--- Code/annotate/remove_field.py
+++ Code/annotate/remove_field.py
@@ -4,10 +4,10 @@ from pymongo import MongoClient
 myclient = MongoClient("mongodb+srv://HonourThesis:XZJXwB8NNdHIoxGw@cluster0.no1barz.mongodb.net/test")
 
 # Choose database and collection
-db = myclient['GSoC']
+db = myclient['Data']
 mycollection = db['Users']
 
 # Remove the field from all documents in the collection
-mycollection.update_many({}, {"$unset": {"comment": 1}})
+mycollection.update_many({}, {"$unset": {"": 1}})
 
 print("finished")
